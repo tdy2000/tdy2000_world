6be2ea1e1e38cf16b6951d42bff55f2c9b3b7260
Dennis Lo <dlo2@andrew.cmu.edu>
2021-03-10 13:00:00 -0500

    Add controller and views for Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
-  belongs_to :category
+  has_one :category
 
   validates_presence_of :title, :content,
     allow_blank: false
 
-  scope :alphabetical, ->{ order(title: :asc) }
+  scope :alphabetical, ->{ order('title') }
   scope :active, ->{ where(active: true) }
 end
